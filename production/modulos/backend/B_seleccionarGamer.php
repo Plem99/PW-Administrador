e0df67c86f7707a14c0ddf25c9f36569a0af0ebf
plem99 <1730314@upv.edu.mx>
2020-04-11 17:42:45 -0500

    Parte de Carlos

--- production/modulos/backend/B_seleccionarGamer.php
+++ production/modulos/backend/B_seleccionarGamer.php
@@ -5,13 +5,4 @@
 	$result = $db->query($sql);
 	$row = $result->fetch(PDO::FETCH_ASSOC);
 
-	if($_POST['val']==2){
-		index();
-	}
-
-	function index(){
-		$sql2 = "SELECT * FROM gamers";
-		$result2 = $db->query($sql2);
-		$row2 = $result2->fetch(PDO::FETCH_ASSOC);
-	}
 ?>
