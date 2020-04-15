ce39a12fa6da13e13f7ba82e1d907dfc5227c287
plem99 <1730314@upv.edu.mx>
2020-04-15 11:30:32 -0500

    Dulceria FULL

--- production/modulos/backend/B_registroVentas.php
+++ production/modulos/backend/B_registroVentas.php
@@ -5,12 +5,12 @@
 	if ($result = $db->query($sql)) {
 	    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
 	        echo "<tr>
-		        	<td>".$row["name"]."</td>
-		        	<td>".$row["candy_name"]."</td>
-		        	<td>".$row["total"]."</td>
-		        	<td>".$row["payment"]."</td>
-		        	<td>".$row["change_purchase"]."</td>
-		        	<td>".$row["date_purchase"]."</td>
+		        	<td>".$row['name']."</td>
+		        	<td>".$row['candy_name']."</td>
+		        	<td>".$row['total']."</td>
+		        	<td>".$row['payment']."</td>
+		        	<td>".$row['change_purchase']."</td>
+		        	<td>".$row['date_purchase']."</td>
 	        	</tr>";
 	    }
 	    echo "</table>";
